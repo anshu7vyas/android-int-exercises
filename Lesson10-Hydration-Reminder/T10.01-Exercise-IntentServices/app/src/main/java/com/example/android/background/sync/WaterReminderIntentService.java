d9f41fac7f77b9065e3182253c9f76b00afb4188
av-7 <amanshulvyas@gmail.com>
2018-03-27 21:41:00 -0700

    completes T10.01-02 PendingIntent and creating notifications

--- Lesson10-Hydration-Reminder/T10.01-Exercise-IntentServices/app/src/main/java/com/example/android/background/sync/WaterReminderIntentService.java
+++ Lesson10-Hydration-Reminder/T10.01-Exercise-IntentServices/app/src/main/java/com/example/android/background/sync/WaterReminderIntentService.java
@@ -14,10 +14,30 @@
  * limitations under the License.
  */
 
-// TODO (9) Create WaterReminderIntentService and extend it from IntentService
+package com.example.android.background.sync;
 
-//  TODO (10) Create a default constructor that calls super with the name of this class
+import android.app.IntentService;
+import android.content.Intent;
+import android.support.annotation.Nullable;
 
-//  TODO (11) Override onHandleIntent
-//      TODO (12) Get the action from the Intent that started this Service
-//      TODO (13) Call ReminderTasks.executeTaskForTag and pass in the action to be performed
+
+// DONE (9) Create WaterReminderIntentService and extend it from IntentService
+public class WaterReminderIntentService extends IntentService {
+
+
+    //  DONE (10) Create a default constructor that calls super with the name of this class
+    public WaterReminderIntentService() {
+        super("WaterReminderIntentService");
+    }
+
+    //  DONE (11) Override onHandleIntent
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        //      DONE (12) Get the action from the Intent that started this Service
+        //      DONE (13) Call ReminderTasks.executeTaskForTag and pass in the action to be performed
+        String action = intent.getAction();
+        ReminderTasks.executeTask(this, action);
+    }
+
+
+}
